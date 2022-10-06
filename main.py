a1affe32dbd9d058ff30bda99b9f92fa35e1e3ec
zhouzi <zijun06>
2022-10-06 16:16:09 -0400

    reglement des condition du choix

--- main.py
+++ main.py
@@ -1,30 +1,16 @@
 """
 Le but de la partie est d’accumuler le plus possible de victoires.  Elles s’additionnent en vainquant des monstres.
-  Un personnage circule dans un couloir (le monde), à chaque extrémité du couloir il y a une porte.
-   Chacune de ces portes mène à une salle (le niveau).
-   Dans chaque salle, il y a un monstre et chaque monstre a un niveau de difficulté qui lui est propre.
+ Un personnage circule dans un couloir (le monde), à chaque extrémité du couloir il y a une porte.
+  Chacune de ces portes mène à une salle (le niveau).
+  Dans chaque salle, il y a un monstre et chaque monstre a un niveau de difficulté qui lui est propre.
 """
 import random
 
-niveau_vie = 20
-score_de = random.randint(1, 6)
-numero_adversaire = 0
-numero_combat = 0
-nombre_victoires = 0
-nombre_defaites = 0
-combat_statut = "aucune battaille efectuer encore pour le moment..."
-
-force_adversaire = random.randint(1, 5)
-
-
 def apparaitre_stat():
-    print(f"Adversaire :{numero_adversaire}\n"
-          f"Force de l’adversaire :_{force_adversaire}\n"
-          f"Niveau de vie de l’usager :_{niveau_vie}\n"
-          f"Combat {numero_combat} :_{nombre_victoires} vs {nombre_defaites}\n"
-          f"Lancer du de:_{score_de}\n"
-          f"Dernier combat = {combat_statut}\n")
-    str(input("espace pour continuer...\n"))
+    print(f"numero de l'adversaire :{numero_adversaire}\n"
+          f"Niveau de vie:_{niveau_vie}\n"
+          f"Combat {numero_combat} :_nombre de victoire: {nombre_victoires} vs nombre de defait: {nombre_defaites}\n")
+    #str(input("espace pour continuer...\n"))
 
 
 def quiter():
@@ -34,7 +20,12 @@ def quiter():
 def apparaitre_regle_du_jeux():
     print("Pour réussir un combat, il faut que la valeur du dé lancé soit supérieure à la force de l’adversaire.\n"
           "  Dans ce cas, le niveau de vie de l’usager est augmenté de la force de l’adversaire.\n"
-          "Une défaite a lieu lorsque la valeur du dé lancé par l’usager est inférieure ou égale à la force de "
+          "Une défaite a lieu lorsque la valeur du dé lancé par l’usager est inférieure ou égale à la force de\n "
+          "l’adversaire.\n "
+          "Dans ce cas, le niveau de vie de l’usager est diminué de la force de l’adversaire.\n"
+          "La partie se termine lorsque les points de vie de l’usager tombent sous 0.\n"
+          "L’usager peut combattre ou éviter chaque adversaire, dans le cas de l’évitement, il y a une pénalité \n"
+          "de 1 point de vie.\n"
           "l’adversaire.\n "
           "Dans ce cas, le niveau de vie de l’usager est diminué de la force de l’adversaire.\n"
           "La partie se termine lorsque les points de vie de l’usager tombent sous 0.\n"
@@ -50,38 +41,78 @@ def menu():
 
 
 while True:
-    print(f"Vous tombez face à face avec un adversaire de difficulté :_{force_adversaire}\n")
-    menu()
-    choix = int(input("Que voulez-vous faire ?"))
+    niveau_vie = 1
+    numero_adversaire = 0
+    numero_combat = 0
+    nombre_victoires = 0
+    nombre_defaites = 0
+    combat_statut = "aucune battaille efectuer encore pour le moment..."
+    nombre_victoires_consecutives = 0
 
-    if choix == 1:
-        apparaitre_stat()
-        if force_adversaire >= score_de:
-            niveau_vie -= force_adversaire
-            combat_statut = "defaites"
-            numero_adversaire += 1
-            numero_combat += 1
-            nombre_defaites += 1
+    while niveau_vie > 0:
+        force_adversaire = random.randint(1, 5)
+        print(f"Vous tombez face à face avec un adversaire de difficulté : {force_adversaire}\n")
+        menu()
+        choix = int(input("Que voulez-vous faire ?\n"))
 
-            if niveau_vie <= 0:
-                print()
-        else:
-            niveau_vie += force_adversaire
-            combat_statut = "victoire"
-            numero_adversaire += 1
-            numero_combat += 1
-            nombre_victoires += 1
+        while choix != 0:
+            if choix == 1:
+                numero_adversaire += 1
+                numero_combat += 1
+                apparaitre_stat()
+                score_de = random.randint(1, 6)
+
+                if force_adversaire >= score_de:
+                    print(f"force_adversaire:{force_adversaire}\n"
+                          f"Lancer du dé : {score_de}")
+                    niveau_vie -= force_adversaire
+                    combat_statut = "defaites"
+                    nombre_defaites += 1
+                    nombre_victoires_consecutives = 0
+                    print(f"Dernier combat = {combat_statut}")
+                    print(f"Niveau de vie :{niveau_vie}")
+
+                    #input("espace pour continuer...\n")
+                    break
+
+                else:
+                    print(f"Lancer du dé : {score_de}")
+                    niveau_vie += force_adversaire
+                    nombre_victoires_consecutives += 1
+                    combat_statut = "victoire"
+                    nombre_victoires += 1
+                    print(f"Dernier combat = {combat_statut}")
+                    print(f"Niveau de vie : {niveau_vie} \n"
+                          f"Nombre de victoires consécutives : {nombre_victoires_consecutives}")
+                    #str(input("espace pour continuer...\n"))
+                    break
+
+            elif choix == 2:
+
+                niveau_vie -= 1
+                print("pénalité de 1 point de vie, vous avez reussi a contourner le monstre.")
+                #str(input("espace pour continuer...\n"))
+                break
+
+            elif choix == 3:
+                apparaitre_regle_du_jeux()
+                #str(input("espace pour continuer...\n"))
+                break
 
 
-    elif choix == 2:
-        print("2")
+            elif choix == 4:
+                quiter()
+                break
 
-    elif choix == 3:
-        apparaitre_regle_du_jeux()
+            else:
+                print(";-;\n"
+                      "silvous plait recomencer...\n")
+                break
 
-    elif choix == 4:
-        quiter()
-        break
     else:
-        print(";-;\n"
-              "silvous plait recomencer...\n")
+        print(f"La partie est terminée, vous avez vaincu {nombre_victoires} monstre(s).")
+        nouveaux_partie = str(input("voulez-vous recomencer?_"))
+        if nouveaux_partie == "o":
+            continue
+        else:
+            break
