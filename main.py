7cc9eaf5a40e8e1d49a8d7a4f0374660467127e2
zhouzi <zijun06>
2022-10-14 10:44:25 -0400

    boss figth

--- main.py
+++ main.py
@@ -44,6 +44,66 @@ def choix_du_joueur():
                       "4- Quitter la partie\n"))
 
 
+def boss_figth():
+    global niveau_vie
+    global boss_figth_win
+    global score_de
+    global nombre_victoires_consecutives
+    global nombre_victoires
+
+    boss_vie = 3
+    while boss_vie != 0:
+
+        boss_figth_choix = 0
+        boss_force = random.randint(1, 5)
+        print(f"vous tomber devant le roi des monstre!!!\n"
+              f"il est beaucoup plus fort que les autre!!!\n"
+              f"Ici vous avez seulement deux choix: "
+              f"1- pour attaquer "
+              f"2- pour contourner l'attaque du Boss"
+              f"lorsque votre lancer est 1 ou 6, le Boss perte un point de vie.\n"
+              f"le Boss a entout 3 point de vie.\n"
+              f"Bonne Chance!!!\n")
+        boss_figth_choix = int(input(f"Quel est votre choix?\n"
+                                     f"1- attaquer\n"
+                                     f"2- contourner l'attaque du Boss"))
+        if boss_figth_choix == 1:
+            score_de = random.randint(1, 6)
+            print(f"votre lancer: {score_de}")
+            if score_de == 6 or score_de == 1:
+                boss_vie -= 1
+                print(f"la vie du boss: {boss_vie}")
+            else:
+                niveau_vie -= boss_force
+                print(f"Oh No!!!\n")
+                print(f"votre niveau de vie: {niveau_vie}\n")
+                if niveau_vie < 0:
+                    print(f"You die...")
+                    boss_figth_win = False
+                    break
+
+        elif boss_figth_choix == 2:
+            print(f"vous avez reussi de contourner l'attaque du monstre!")
+
+        else:
+            niveau_vie -= boss_force
+            print(f"Oh No!!!\n")
+            print(f"erreur inatandu!!!\n")
+            print(f"votre niveau de vie: {niveau_vie}\n")
+    else:
+        boss_figth_win = True
+        print("Vous avez gagner la battaille...")
+        apparaitre_stat()
+        niveau_vie += force_adversaire
+        nombre_victoires_consecutives += 1
+        combat_statut = "victoire"
+        nombre_victoires += 1
+        print(f"Dernier combat = {combat_statut}")
+        print(f"Niveau de vie : {niveau_vie} \n"
+              f"Nombre de victoires consécutives : {nombre_victoires_consecutives}\n")
+
+
+
 
 play_game = True
 while play_game:
@@ -57,12 +117,14 @@ while play_game:
     nombre_victoires_consecutives = 0
     afficher_regle = False
     force_adversaire = 0
+    boss_figth_win = None
 
     while niveau_vie > 0:
         if not afficher_regle:
             force_adversaire = random.randint(1, 5)
         else:
             afficher_regle = False
+            print(afficher_regle)
 
         print(f"Vous tombez face à face avec un adversaire de difficulté : {force_adversaire}\n")
         choix_du_joueur()
@@ -92,6 +154,10 @@ while play_game:
                 print(f"Dernier combat = {combat_statut}")
                 print(f"Niveau de vie : {niveau_vie} \n"
                       f"Nombre de victoires consécutives : {nombre_victoires_consecutives}\n")
+
+                if nombre_victoires_consecutives % 3 == 0:
+
+                    boss_figth()
 
         elif choix == 2:
             niveau_vie -= 1
