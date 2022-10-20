ed5c47ca31a0581cf0583287c474918a35c4a5fc
zhouzi <zijun06>
2022-10-20 16:01:47 -0400

    Boss fight fini

--- main.py
+++ main.py
@@ -10,16 +10,28 @@ import random
 
 
 def apparaitre_stat():
+    """
+    Apparaitre le information du joueur.
+    :return:
+    """
     print(f"numero de l'adversaire :{numero_adversaire}\n"
           f"Niveau de vie:_{niveau_vie}\n"
           f"Combat {numero_combat} :_nombre de victoire: {nombre_victoires} vs nombre de defait: {nombre_defaites}\n")
 
 
 def quiter():
+    """
+    Quitter le jeu.
+    :return:
+    """
     print("Merci et au revoir...")
 
 
 def apparaitre_regle_du_jeux():
+    """
+    Apparaitre le reglement du jeu.
+    :return:
+    """
     print("Pour réussir un combat, il faut que la valeur du dé lancé soit supérieure à la force de l’adversaire.\n"
           "  Dans ce cas, le niveau de vie de l’usager est augmenté de la force de l’adversaire.\n"
           "Une défaite a lieu lorsque la valeur du dé lancé par l’usager est inférieure ou égale à la force de\n "
@@ -36,6 +48,10 @@ def apparaitre_regle_du_jeux():
 
 
 def choix_du_joueur():
+    """
+    Returner la valeur entrer par le joueur.
+    :return:
+    """
     global choix
     choix = int(input("Que voulez-vous faire ?\n"
                       "1- Combattre cet adversair\n"
@@ -44,46 +60,64 @@ def choix_du_joueur():
                       "4- Quitter la partie\n"))
 
 
-def boss_figth():
+def comencer_boss_figth():
+    """
+    Commencer le combat avec le boss.
+    :return:
+    """
+
     global niveau_vie
-    global boss_figth_win
+    global combat_statut
     global score_de
     global nombre_victoires_consecutives
     global nombre_victoires
-
+    boss_afficher_regle = True
     boss_vie = 3
     while boss_vie != 0:
-
         boss_figth_choix = 0
-        boss_force = random.randint(1, 5)
-        print(f"vous tomber devant le roi des monstre!!!\n"
-              f"il est beaucoup plus fort que les autre!!!\n"
-              f"Ici vous avez seulement deux choix: "
-              f"1- pour attaquer "
-              f"2- pour contourner l'attaque du Boss"
-              f"lorsque votre lancer est 1 ou 6, le Boss perte un point de vie.\n"
-              f"le Boss a entout 3 point de vie.\n"
-              f"Bonne Chance!!!\n")
-        boss_figth_choix = int(input(f"Quel est votre choix?\n"
-                                     f"1- attaquer\n"
-                                     f"2- contourner l'attaque du Boss"))
+
+        if not boss_afficher_regle:
+            boss_force = random.randint(1, 5)
+            boss_figth_choix = int(input(f"Quel est votre choix?\n"
+                                         f"1- attaquer\n"
+                                         f"2- contourner l'attaque du Boss\n"
+                                         f"3- afficher regle\n"))
+        else:
+
+            boss_force = random.randint(1, 5)
+            print(f"vous tomber devant le roi des monstre!!!\n"
+                  f"Ici, vous avez seulement deux choix: \n"
+                  f"1- pour attaquer \n"
+                  f"2- pour contourner l'attaque du Boss\n"
+                  f"lorsque votre lancer est 1 ou 6, le Boss perte un point de vie.\n"
+                  f"le Boss a entout 3 point de vie.\n"
+                  f"Bonne Chance!\n")
+            boss_afficher_regle = False
+
+            boss_figth_choix = int(input(f"Quel est votre choix?\n"
+                                         f"1- attaquer\n"
+                                         f"2- contourner l'attaque du monstre\n"))
+
         if boss_figth_choix == 1:
             score_de = random.randint(1, 6)
-            print(f"votre lancer: {score_de}")
+            print(f"votre lancer: {score_de}\n")
             if score_de == 6 or score_de == 1:
                 boss_vie -= 1
-                print(f"la vie du boss: {boss_vie}")
+                print(f"vous avez reussi a endomager le roi du montre!\n"
+                      f"la vie du boss: {boss_vie}\n")
             else:
                 niveau_vie -= boss_force
-                print(f"Oh No!!!\n")
-                print(f"votre niveau de vie: {niveau_vie}\n")
+                print(f"Oh No!!!\n"
+                      f"votre niveau de vie: {niveau_vie}\n")
                 if niveau_vie < 0:
-                    print(f"You die...")
-                    boss_figth_win = False
+                    print(f"Vous etes mort...")
                     break
 
         elif boss_figth_choix == 2:
-            print(f"vous avez reussi de contourner l'attaque du monstre!")
+            print(f"vous avez reussi de contourner l'attaque du monstre!\n")
+
+        elif boss_figth_choix == 3:
+            boss_afficher_regle = True
 
         else:
             niveau_vie -= boss_force
@@ -91,18 +125,15 @@ def boss_figth():
             print(f"erreur inatandu!!!\n")
             print(f"votre niveau de vie: {niveau_vie}\n")
     else:
-        boss_figth_win = True
-        print("Vous avez gagner la battaille...")
+        print("Vous avez gagner la battaille!")
         apparaitre_stat()
         niveau_vie += force_adversaire
         nombre_victoires_consecutives += 1
         combat_statut = "victoire"
         nombre_victoires += 1
-        print(f"Dernier combat = {combat_statut}")
+        print(f"Dernier combat = {combat_statut}\n")
         print(f"Niveau de vie : {niveau_vie} \n"
               f"Nombre de victoires consécutives : {nombre_victoires_consecutives}\n")
-
-
 
 
 play_game = True
@@ -117,7 +148,6 @@ while play_game:
     nombre_victoires_consecutives = 0
     afficher_regle = False
     force_adversaire = 0
-    boss_figth_win = None
 
     while niveau_vie > 0:
         if not afficher_regle:
@@ -155,9 +185,8 @@ while play_game:
                 print(f"Niveau de vie : {niveau_vie} \n"
                       f"Nombre de victoires consécutives : {nombre_victoires_consecutives}\n")
 
-                if nombre_victoires_consecutives % 3 == 0:
-
-                    boss_figth()
+                if nombre_victoires % 3 == 0 and nombre_victoires != 0:
+                    comencer_boss_figth()
 
         elif choix == 2:
             niveau_vie -= 1
